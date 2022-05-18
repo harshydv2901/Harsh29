d8e89a0bc7b6d4de755ddbb22a9d8c560d7535ed
unknown <you@example.com>
2022-05-18 08:16:06 +0000

    demo

---
+++ AddNum.java
@@ -0,0 +1,14 @@
+package AddTwoNumber;
+
+public class AddNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 = 10;
+		int number2 = 20;
+		int result = number1 + number2;
+
+		System.out.print("Sum of two numbers " + result);
+	}
+
+}
